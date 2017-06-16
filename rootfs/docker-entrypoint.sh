1cfe3d12e86d671faa133bf787845e9f1ec32f0d
Anu <anusree.kailash@cdnetworks.com>
2017-06-16 15:19:24 +0900

    config file fix

--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -4,7 +4,7 @@ cat /etc/alertmanager/config.yml | \
     sed "s@#api_url: 'null'#@api_url: '$SLACK_API'@g" |\
     sed "s@#room_id: 'null'#@room_id: '$HIPCHAT_ROOM'@g" |\
     sed "s@#auth_token: 'null'#@auth_token: '$HIPCHAT_TOKEN'@g" |\
-    sed "s@#url: 'null'#@url: '$HIPCHAT_URL'@g" |\
+    sed "s@#hipchat_url: 'null'#@hipchat_url: '$HIPCHAT_URL'@g" |\
     sed "s@#logstash_url: 'null'#@url: '$LOGSTASH_URL'@g" > /tmp/config.yml
 
 mv /tmp/config.yml /etc/alertmanager/config.yml
