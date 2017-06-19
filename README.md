409986c961f65258986d80039fabe1f8a1178c2f
Anu <anusree.kailash@cdnetworks.com>
2017-06-19 14:46:16 +0900

    Single route configuration

--- README.md
+++ README.md
@@ -7,9 +7,11 @@ It has 3 different receivers configured: hipchat, logstash and slack.
 ## How to run it
 
 You need to provide a valid slack api key and logstash endpoint and hipchat room id, token and url.
+The only receiver to be used will be the one configured as $DEFAULT (hipchat, slack or logstash).
 Both can be passed to the container as an environmental variable. For example:
 
     docker run \
+      -e "DEFAULT=hipchat" \
       -e "HIPCHAT_ROOM=12345" \
       -e "HIPCHAT_TOKEN=XXXXXXXXX" \
       -e "HIPCHAT_URL=XXXXXXXXX" \
--- rootfs/docker-entrypoint.sh
+++ rootfs/docker-entrypoint.sh
@@ -1,10 +1,11 @@
 #!/bin/sh -e
 
 cat /etc/alertmanager/config.yml | \
-    sed "s@#api_url: 'null'#@api_url: '$SLACK_API'@g" |\
+    sed "s@#receiver: 'default'#@receiver: '$DEFAULT'@g" |\
     sed "s@#room_id: 'null'#@room_id: '$HIPCHAT_ROOM'@g" |\
     sed "s@#auth_token: 'null'#@auth_token: '$HIPCHAT_TOKEN'@g" |\
     sed "s@#hipchat_url: 'null'#@hipchat_url: '$HIPCHAT_URL'@g" |\
+    sed "s@#api_url: 'null'#@api_url: '$SLACK_API'@g" |\
     sed "s@#logstash_url: 'null'#@url: '$LOGSTASH_URL'@g" > /tmp/config.yml
 
 mv /tmp/config.yml /etc/alertmanager/config.yml
